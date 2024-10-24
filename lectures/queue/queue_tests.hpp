5975fb55e63767fdc6862597a0baf84843673d75
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2024-10-24 12:37:02 +0300

    Първоначална реализация на статична опашка

--- lectures/queue/queue_tests.hpp
+++ lectures/queue/queue_tests.hpp
@@ -2,3 +2,42 @@
 #include "squeue.hpp"
 
 #define QUEUES StaticQueue<int>
+
+TEST_CASE_TEMPLATE("Създаване на празна опашка", Queue, QUEUES) {
+    Queue q;
+    CHECK(q.empty());
+}
+
+TEST_CASE_TEMPLATE("При добавяне на елемнт в опашката, тя не е празна", Queue, QUEUES) {
+    Queue q;
+    q.enqueue(1);
+    CHECK(!q.empty());
+}
+
+TEST_CASE_TEMPLATE("Опит за изключване на елемент от празна опашка", Queue, QUEUES) {
+    Queue q;
+    CHECK_THROWS(q.dequeue());
+}
+
+TEST_CASE_TEMPLATE("Опит за поглеждане на елемент в празна опашка", Queue, QUEUES) {
+    Queue q;
+    CHECK_THROWS(q.head());
+}
+
+TEST_CASE_TEMPLATE("При добавяне на елемнт в опашката, той е първи", Queue, QUEUES) {
+    Queue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    CHECK(q.head() == 1);
+}
+
+TEST_CASE_TEMPLATE("При добавяне на елементи в опашката, те излизат в същия ред", Queue, QUEUES) {
+    Queue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    CHECK(q.dequeue() == 1);
+    CHECK(q.dequeue() == 2);
+    CHECK(q.dequeue() == 3);
+}
--- lectures/queue/squeue.hpp
+++ lectures/queue/squeue.hpp
@@ -5,19 +5,40 @@
 template <typename T>
 class StaticQueue {
 public:
-    StaticQueue() {}
+    // капацитет на опашката
+    static const size_t MAX = 100;
+private:
+    // данните на опашката
+    T data[MAX];
+    // начало и край на опашката
+    int front, back;
+public:
+
+    StaticQueue() : front(0), back(-1) {}
 
     // проверка дали опашката е празна
-    bool empty() const { throw std::runtime_error("Не е реализирано"); }
+    bool empty() const { return front > back; }
 
     // включване на елемент в опашката 
-    void enqueue(T const& x) { throw std::runtime_error("Не е реализирано"); }
+    void enqueue(T const& x) {
+        if (back == MAX - 1)
+            throw std::runtime_error("Опит за включване в пълна опашка");
+        data[++back] = x;
+    }
 
     // изключване на елемент от опашката
-    T dequeue() { throw std::runtime_error("Не е реализирано"); }
+    T dequeue() {
+        if (empty())
+            throw std::runtime_error("Опит за изключване от празна опашка");
+        return data[front++];
+    }
 
     // първият елемент в опашката
-    T const& head() const { throw std::runtime_error("Не е реализирано"); }
+    T const& head() const {
+        if (empty())
+            throw std::runtime_error("Опит за поглеждане в празна опашка");
+        return data[front];
+    }
 };
 
 #endif // SQUEUE_HPP
