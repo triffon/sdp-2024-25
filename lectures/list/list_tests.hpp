ff8ed51ef04645fd34f83087ce53f19d304ec9fd
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2024-11-07 12:49:01 +0200

    Тест за insertAfter

--- lectures/list/list_tests.hpp
+++ lectures/list/list_tests.hpp
@@ -33,3 +33,19 @@ TEST_CASE_TEMPLATE("Създаване на списък с числата от 
         CHECK(j == i++);
     CHECK(i == 11);
 }
+
+TEST_CASE_TEMPLATE("Създаване на списък с нечетните числа от 1 до 10 и вмъкване на четните числа между тях с insertAfter", SomeList, LISTS) {\
+    SomeList list;
+
+    for (int i = 1; i <= 10; i += 2)
+        list.insertLast(i);
+
+    typename SomeList::Iterator it = list.begin();
+    for (int i = 2; i <= 10; i += 2, ++(++it))
+        list.insertAfter(i, it);
+
+    int i = 1;
+    for (int j : list)
+        CHECK(j == i++);
+    CHECK(i == 11);
+}
